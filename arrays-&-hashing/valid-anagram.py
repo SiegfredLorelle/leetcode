26ecdab77ddac237643eed3f49f8b1fdef8bf7f8
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-03-18 05:48:16 +0800

    include another solution in valid anagram

--- arrays-&-hashing/valid-anagram.py
+++ arrays-&-hashing/valid-anagram.py
@@ -1,21 +1,46 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_hash, t_hash = {}, {}
+        """ Solution using one hashmap """
+        hash = {}
 
         if len(s) != len(t):
             return False
 
-        for i in range(len(s)):
-            s_hash[s[i]] = 1 + s_hash.get(s[i], 0)
-            t_hash[t[i]] = 1 + t_hash.get(t[i], 0)
-        
-        if len(s_hash) != len(t_hash):
-            return False
+        for i in s:
+            if i in hash:
+                hash[i] += 1
+            else:
+                hash[i] = 1
 
-        for i in s_hash:
-            if s_hash[i] != t_hash.get(i, 0):
+        for i in t:
+            if i not in hash:
+                return False
+            hash[i] -= 1
+
+        for i in hash:
+            if hash[i] != 0:
                 return False
         return True
+
+
+
+        """ Solution using two hashmaps """
+        # s_hash, t_hash = {}, {}
+
+        # if len(s) != len(t):
+        #     return False
+
+        # for i in range(len(s)):
+        #     s_hash[s[i]] = 1 + s_hash.get(s[i], 0)
+        #     t_hash[t[i]] = 1 + t_hash.get(t[i], 0)
+        
+        # if len(s_hash) != len(t_hash):
+        #     return False
+
+        # for i in s_hash:
+        #     if s_hash[i] != t_hash.get(i, 0):
+        #         return False
+        # return True
 
 
 

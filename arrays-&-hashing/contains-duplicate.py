0bda6f392bd130ba329f904121aff95f8068e08b
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-03-19 11:01:17 +0800

    add descriptions via docstrings in contains duplicate, two sum, and valid anagram

--- arrays-&-hashing/contains-duplicate.py
+++ arrays-&-hashing/contains-duplicate.py
@@ -1,3 +1,11 @@
+"""
+Contains Duplicate
+
+Given an integer array nums, return true if any value appears
+at least twice in the array, and return false if every element is distinct.
+"""
+
+
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
         hashset = set()

c96978bbdeaddffcf2dddc506068a797557c3778
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-03-23 23:45:55 +0800

    add another solution for consecutive seq

--- arrays-&-hashing/longest-consecutive-sequence.py
+++ arrays-&-hashing/longest-consecutive-sequence.py
@@ -10,26 +10,50 @@ You must write an algorithm that runs in O(n) time.
 
 class Solution:
     def longestConsecutive(self, nums: list[int]) -> int:
+
+        """ SOLUTION USING SORT """
+        # nums = set(nums)
+
+        # if len(nums) <= 1:
+        #     return len(nums)
+
+        # nums = sorted(nums)
+
+        # res = 0
+        # count = 1
+        # for index in range(len(nums) - 1):
+        #     if abs(nums[index] - nums[index + 1]) == 1:
+        #         count += 1
+        #     else:
+        #         if count > res:
+        #             res = count
+        #         count = 1
+
+        # if count > res:
+        #     res = count
+        # return res
+
+
+        """ SOLUTION BY COUNTING SEQUENCES """
         nums = set(nums)
 
         if len(nums) <= 1:
             return len(nums)
 
-        nums = sorted(nums)
+        longest = 0
+        for num in nums:
+            # Check if starting sequence
+            if num - 1 not in nums:
+                    length = 1
+                    while num + length in nums:
+                        length += 1
+                    
+                    if longest < length:
+                        longest = length 
 
-        res = 0
-        count = 1
-        for index in range(len(nums) - 1):
-            if abs(nums[index] - nums[index + 1]) == 1:
-                count += 1
-            else:
-                if count > res:
-                    res = count
-                count = 1
+        return longest
+        
 
-        if count > res:
-            res = count
-        return res
 
 
 sol = Solution()

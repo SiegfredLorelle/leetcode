87d421806d8f64794f9ae85f15c94befbac1cb0e
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-05-04 08:35:24 +0800

    Solve Find the Duplicate Number Problem
    The problem is classified as Medium and is under linked list section.
    I was not able to solve the solution on my own. It is listed to
    need-more-prac.txt.

---
+++ linked-list/find-the-duplicate-number.py
@@ -0,0 +1,39 @@
+"""Find the Duplicate Number
+
+Given an array of integers nums containing n + 1 integers 
+where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums 
+and uses only constant extra space.
+"""
+
+from typing import List
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = 0
+        fast = 0
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        slow2 = 0
+
+        while slow != slow2:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+
+        return slow
+
+
+
+
+
+sol = Solution()
+result = sol.findDuplicate([1,3,4,2,2])
+print(result)

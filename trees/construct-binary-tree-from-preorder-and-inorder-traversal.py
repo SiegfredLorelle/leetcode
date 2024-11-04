f23b94d8bc9c0043b92a5c0bd17164bffcbf33af
SiegfredLorelle <siegfredlorelle09@gmail.com>
2024-11-04 09:15:35 +0800

    Add another sol for Construct BST from Pre and In

--- trees/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ trees/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -15,14 +15,38 @@ class TreeNode:
         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not inorder or not preorder:
-            return
+        """ SOLUTION USING DFS: quadratic space and time (n^2) """
+        # if not inorder or not preorder:
+        #     return
 
-        numLeftNodes = inorder.index(preorder[0]) + 1
+        # numLeftNodes = inorder.index(preorder[0]) + 1
 
-        newNode = TreeNode()
-        newNode.val = preorder[0]
-        newNode.left = self.buildTree(preorder[1:numLeftNodes], inorder[:numLeftNodes - 1])
-        newNode.right = self.buildTree(preorder[numLeftNodes :], inorder[numLeftNodes:])
+        # newNode = TreeNode()
+        # newNode.val = preorder[0]
+        # newNode.left = self.buildTree(preorder[1:numLeftNodes], inorder[:numLeftNodes - 1])
+        # newNode.right = self.buildTree(preorder[numLeftNodes :], inorder[numLeftNodes:])
 
-        return newNode
+        # return newNode
+    
+        """ SOLUTION USING DFS: linear space and time (n) """
+        # Inorder values mapping to its index
+        inIdxs = {num: idx for idx, num in enumerate(inorder)}
+        # Preorder index, used for during dfs traversal
+        self.preIdx = 0
+
+        def dfs(start, end):
+            # Base case
+            if start > end:
+                return
+
+            # Get the middle, separates left vals and right vals
+            mid = inIdxs[preorder[self.preIdx]]
+            
+            # Build the node
+            node = TreeNode(preorder[self.preIdx])
+            self.preIdx += 1
+            node.left = dfs(start, mid - 1)
+            node.right = dfs(mid + 1, end)
+            return node   
+        
+        return dfs(0, len(preorder) - 1)

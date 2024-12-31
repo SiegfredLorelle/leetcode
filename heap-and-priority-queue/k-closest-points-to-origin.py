5af93c1e1fb511242931116eefd71410f41523fd
Siegfred Lorelle <siegfredlorelle09@gmail.com>
2024-12-31 16:24:57 +0800

    Add another solution to K Closest Points to Origin

--- heap-and-priority-queue/k-closest-points-to-origin.py
+++ heap-and-priority-queue/k-closest-points-to-origin.py
@@ -15,6 +15,11 @@ import heapq
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+    """ SOLUTION USING SORT """
+    # points.sort(key=lambda point: point[0] ** 2 + point[1] ** 2)
+    # return points[:k]
+
+    """ SOLUTION USING MAX HEAP """
         maxHeap = []
         for point in points:
             x, y = point

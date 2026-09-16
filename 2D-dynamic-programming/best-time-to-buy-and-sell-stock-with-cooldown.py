8b41eb80a69b7e449365bb3253b81c54ecb4c09f
Siegfred Lorelle Mina <siegfredlorelle09@gmail.com>
2026-09-17 03:33:09 +0800

    2D-DP: add a more optimized solution for Best Time to Buy and Sell Stock with Cooldown

--- 2D-dynamic-programming/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 2D-dynamic-programming/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -19,25 +19,48 @@ from type import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        memo = {}
+    # Bottom Up - time: O(n) space: O(1)
+    # buy -> sell -> hold -> buy
+    prev_sell = 0
+    prev_hold = 0
+    prev_buy = 0
 
-        def dfs(i, is_buying):
-            if i >= len(prices):
-                return 0
+    for idx in range(len(prices) - 1, -1, -1):
+        # selling only happens after buying
+        curr_sell = prev_buy
+        # holding happens after selling
+        curr_hold = max(prev_hold, prices[idx] + prev_sell)
+        # buying happens after holding
+        curr_buy = max(prev_buy, -prices[idx] + prev_hold)
 
-            if (key := (i, is_buying)) in memo:
-                return memo[key]
+        prev_sell = curr_sell
+        prev_hold = curr_hold
+        prev_buy = curr_buy
 
-            cd = dfs(i + 1, is_buying)
-            if is_buying:
-                buy = dfs(i + 1, not is_buying) - prices[i]
-                memo[key] = max(buy, cd)
-            else:
-                sell = dfs(i + 2, not is_buying) + prices[i]
-                memo[key] = max(sell, cd)
-
-            return memo[key]
+    return prev_buy
 
 
-        return dfs(0, True)
+
+    # Top Down - time: O(n) space: O(n)
+    # memo = {}
+
+    # def dfs(i, is_buying):
+    #     if i >= len(prices):
+    #         return 0
+
+    #     if (key := (i, is_buying)) in memo:
+    #         return memo[key]
+
+    #     cd = dfs(i + 1, is_buying)
+    #     if is_buying:
+    #         buy = dfs(i + 1, not is_buying) - prices[i]
+    #         memo[key] = max(buy, cd)
+    #     else:
+    #         sell = dfs(i + 2, not is_buying) + prices[i]
+    #         memo[key] = max(sell, cd)
+
+    #     return memo[key]
+
+
+    # return dfs(0, True)
 
